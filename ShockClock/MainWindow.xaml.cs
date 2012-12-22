0ad000fb69fce9ea5c9cae20af8d111125726512
ShawnFeatherly <feddas00@gmail.com>
2012-12-21 17:17:52 -0800

    First move toards MVVM

--- ShockClock/MainWindow.xaml.cs
+++ ShockClock/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Shapes;
 using System.Runtime.InteropServices;
 using System.Windows.Interop;
 using System.Windows.Threading;
+using ShockClock.ViewModel;
 
 namespace ShockClock
 {
@@ -22,23 +23,20 @@ namespace ShockClock
     /// </summary>
     public partial class MainWindow : Window
     {
-        private DispatcherTimer countdownTimer;
-
-        public int Minutes
+        /// <summary> ViewModel defined in the XAML won't be instantiated </summary>
+        public MainWindowViewModel ViewModelBehind
         {
-            get { return (int)GetValue(MinutesProperty); }
-            set { SetValue(MinutesProperty, value); }
+            get
+            {
+                if (_viewModel == null && this.LayoutRoot != null)
+                {
+                    _viewModel = (MainWindowViewModel)this.LayoutRoot.DataContext;
+                }
+                return _viewModel;
+            }
         }
-        public static readonly DependencyProperty MinutesProperty =
-            DependencyProperty.Register("Minutes", typeof(int), typeof(MainWindow), new UIPropertyMetadata(0));
+        private MainWindowViewModel _viewModel;
 
-        public DateTime EndTime
-        {
-            get { return (DateTime)GetValue(EndTimeProperty); }
-            set { SetValue(EndTimeProperty, value); }
-        }
-        public static readonly DependencyProperty EndTimeProperty =
-            DependencyProperty.Register("EndTime", typeof(DateTime), typeof(MainWindow), new UIPropertyMetadata(DateTime.Now.AddMinutes(30)));
 
         public MainWindow()
         {
@@ -47,25 +45,8 @@ namespace ShockClock
 
         private void Start_Click(object sender, RoutedEventArgs e)
         {
-            if (Settings.Visibility == System.Windows.Visibility.Visible)
-            {
-                Settings.Visibility = System.Windows.Visibility.Collapsed;
-                this.Topmost = true;
-                var hwnd = new WindowInteropHelper(this).Handle;
-                WindowsServices.SetWindowExTransparent(hwnd);
-
-                if (Minutes != 0)
-                    EndTime = DateTime.Now.AddMinutes(Minutes);
-                countdownTimer = new DispatcherTimer();
-                countdownTimer.Interval = TimeSpan.FromSeconds(1);
-                countdownTimer.Tick += new EventHandler(countdownTimer_Tick);
-                countdownTimer.Start();
-            }
-            else
-            {
-                Settings.Visibility = System.Windows.Visibility.Visible;
-                this.Topmost = false;
-            }
+            if (ViewModelBehind != null)
+                this.Topmost = ViewModelBehind.StartClock(this);
         }
 
         private void Exit_Click(object sender, RoutedEventArgs e)
@@ -77,15 +58,12 @@ namespace ShockClock
         private void Maximize_Click(object sender, RoutedEventArgs e)
         {
             Button btnMax = sender as Button;
-            if (WindowState == System.Windows.WindowState.Normal)
+
+            if (ViewModelBehind != null && btnMax != null)
             {
-                WindowState = System.Windows.WindowState.Maximized;
-                if (btnMax != null) btnMax.Content = "Un-Maximize";
-            }
-            else
-            {
-                WindowState = System.Windows.WindowState.Normal;
-                if (btnMax != null) btnMax.Content = "Maximize";
+                string btnText;
+                WindowState = ViewModelBehind.Maximize(WindowState, out btnText);
+                btnMax.Content = btnText;
             }
         }
 
@@ -93,57 +71,5 @@ namespace ShockClock
         {
             DragMove();
         }
-
-        void countdownTimer_Tick(object sender, EventArgs e)
-        {
-            TimeSpan timeLeft = EndTime - DateTime.Now;
-            string minutesRemaining = ((int)timeLeft.TotalMinutes).ToString("00"); //show 2 digits for minutes
-            txtClock.Text = minutesRemaining + timeLeft.ToString(@"\:ss");
-            //txtClock.Text = timeLeft.ToString(@"mm\:ss"); //http://msdn.microsoft.com/en-us/library/ee372287.aspx
-
-            if (timeLeft.TotalSeconds <= 0)
-            {
-                txtClock.Text = "00:00";
-                countdownTimer.Stop();
-            }
-        }
     }
-
-    //http://stackoverflow.com/questions/2842667/how-to-create-a-semi-transparent-window-in-wpf-that-allows-mouse-events-to-pass
-    public static class WindowsServices
-    {
-        const int WS_EX_TRANSPARENT = 0x00000020;
-        const int GWL_EXSTYLE = (-20);
-
-        [DllImport("user32.dll")]
-        static extern int GetWindowLong(IntPtr hwnd, int index);
-
-        [DllImport("user32.dll")]
-        static extern int SetWindowLong(IntPtr hwnd, int index, int newStyle);
-
-        public static void SetWindowExTransparent(IntPtr hwnd)
-        {
-            var extendedStyle = GetWindowLong(hwnd, GWL_EXSTYLE);
-            SetWindowLong(hwnd, GWL_EXSTYLE, extendedStyle | WS_EX_TRANSPARENT);
-        }
-    }
-
-    //public void ClipIt()
-    //{
-    //    ///http://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=4&cad=rja&ved=0CD8QFjAD&url=http%3A%2F%2Fstackoverflow.com%2Fquestions%2F1491115%2Ftemporarily-see-through-wpf-application-a-la-visual-studio-ctrl-for-tooltips&ei=dfRIUKaGJcinqwGYhoHQDg&usg=AFQjCNGh9jpXQdLgcvpxfJ6m0FZCT_SDiw
-    //    IntPtr windowRegion = Control.Windows.CreateRectRgn(0, 0, parkingWindowRect.Width, parkingWindowRect.Height);
-    //            Mubox.Win32.Windows.RECT clipRect = new Mubox.Win32.Windows.RECT();
-
-    //                Mubox.Win32.Windows.GetClientRect(this.Handle, out clipRect);
-    //                clipRect.Left = (parkingWindowRect.Width - clipRect.Width) / 2;
-    //                clipRect.Right += clipRect.Left;
-    //                clipRect.Top = (parkingWindowRect.Height - clipRect.Height) - clipRect.Left;
-    //                clipRect.Bottom = parkingWindowRect.Height - clipRect.Left;
-
-
-    //            IntPtr clipRegion = Control.Windows.CreateRectRgnIndirect(ref clipRect);
-    //            Control.Windows.CombineRgn(windowRegion, windowRegion, clipRegion, Windows.CombineRgnStyles.RGN_XOR);
-    //            Control.Windows.DeleteObject(clipRegion);
-    //            Control.Windows.SetWindowRgn(this.Handle, windowRegion, true);
-    //}
 }
---
+++ ShockClock/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Threading;
+using System.Windows.Interop;
+using System.Runtime.InteropServices;
+
+namespace ShockClock.ViewModel
+{
+    public class MainWindowViewModel : DependencyObject // : INotifyPropertyChanged
+    {
+        //INotifyPropertyChanged
+        //public event PropertyChangedEventHandler PropertyChanged;
+        //protected virtual void OnPropertyChanged(string propertyName)
+        //{
+        //    if (this.PropertyChanged != null)
+        //    {
+        //        this.PropertyChanged(this, new System.ComponentModel.PropertyChangedEventArgs(propertyName));
+        //    }
+        //}
+
+        public string ClockText
+        {
+            get { return (string)GetValue(ClockTextProperty); }
+            set { SetValue(ClockTextProperty, value); }
+        }
+        public static readonly DependencyProperty ClockTextProperty =
+            DependencyProperty.Register("ClockText", typeof(string), typeof(MainWindowViewModel), new UIPropertyMetadata("00:00"));
+
+        public Visibility VisibilityOfSettings
+        {
+            get { return (Visibility)GetValue(VisibilityOfSettingsProperty); }
+            set { SetValue(VisibilityOfSettingsProperty, value); }
+        }
+        public static readonly DependencyProperty VisibilityOfSettingsProperty =
+            DependencyProperty.Register("VisibilityOfSettings", typeof(Visibility), typeof(MainWindowViewModel), new UIPropertyMetadata(Visibility.Visible));
+
+        public int Minutes
+        {
+            get { return (int)GetValue(MinutesProperty); }
+            set { SetValue(MinutesProperty, value); }
+        }
+        public static readonly DependencyProperty MinutesProperty =
+            DependencyProperty.Register("Minutes", typeof(int), typeof(MainWindowViewModel), new UIPropertyMetadata(0));
+
+        public DateTime EndTime
+        {
+            get { return (DateTime)GetValue(EndTimeProperty); }
+            set { SetValue(EndTimeProperty, value); }
+        }
+        public static readonly DependencyProperty EndTimeProperty =
+            DependencyProperty.Register("EndTime", typeof(DateTime), typeof(MainWindowViewModel), new UIPropertyMetadata(DateTime.Now.AddMinutes(30)));
+
+        private DispatcherTimer countdownTimer;
+
+        public MainWindowViewModel()
+        {
+        }
+
+        /// <summary> </summary>
+        /// <returns>true if window should be topmost</returns>
+        public bool StartClock(MainWindow thisHwnd)
+        {
+            if (VisibilityOfSettings == System.Windows.Visibility.Visible)
+            {
+                VisibilityOfSettings = System.Windows.Visibility.Collapsed;
+                var hwnd = new WindowInteropHelper(thisHwnd).Handle;
+                WindowsServices.SetWindowExTransparent(hwnd);
+
+                if (Minutes != 0)
+                    EndTime = DateTime.Now.AddMinutes(Minutes);
+                countdownTimer = new DispatcherTimer();
+                countdownTimer.Interval = TimeSpan.FromSeconds(1);
+                countdownTimer.Tick += new EventHandler(countdownTimer_Tick);
+                countdownTimer.Start();
+                return true;
+            }
+            else
+            {
+                VisibilityOfSettings = System.Windows.Visibility.Visible;
+                return false;
+            }
+        }
+
+        public WindowState Maximize(WindowState winState, out string btnText)
+        {
+            if (winState == System.Windows.WindowState.Normal)
+            {
+                winState = System.Windows.WindowState.Maximized;
+                btnText = "Un-Maximize";
+            }
+            else
+            {
+                winState = System.Windows.WindowState.Normal;
+                btnText = "Maximize";
+            }
+            return winState;
+        }
+
+        void countdownTimer_Tick(object sender, EventArgs e)
+        {
+            TimeSpan timeLeft = EndTime - DateTime.Now;
+            string minutesRemaining = ((int)timeLeft.TotalMinutes).ToString("00"); //show 2 digits for minutes
+            ClockText = minutesRemaining + timeLeft.ToString(@"\:ss");
+            //txtClock.Text = timeLeft.ToString(@"mm\:ss"); //http://msdn.microsoft.com/en-us/library/ee372287.aspx
+
+            if (timeLeft.TotalSeconds <= 0)
+            {
+                ClockText = "00:00";
+                countdownTimer.Stop();
+            }
+        }
+    }
+
+    //http://stackoverflow.com/questions/2842667/how-to-create-a-semi-transparent-window-in-wpf-that-allows-mouse-events-to-pass
+    public static class WindowsServices
+    {
+        const int WS_EX_TRANSPARENT = 0x00000020;
+        const int GWL_EXSTYLE = (-20);
+
+        [DllImport("user32.dll")]
+        static extern int GetWindowLong(IntPtr hwnd, int index);
+
+        [DllImport("user32.dll")]
+        static extern int SetWindowLong(IntPtr hwnd, int index, int newStyle);
+
+        public static void SetWindowExTransparent(IntPtr hwnd)
+        {
+            var extendedStyle = GetWindowLong(hwnd, GWL_EXSTYLE);
+            SetWindowLong(hwnd, GWL_EXSTYLE, extendedStyle | WS_EX_TRANSPARENT);
+        }
+    }
+}
